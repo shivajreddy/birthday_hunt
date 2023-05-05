b0c64f7c1904baa972b7aae16dfad9a94c2e82d4
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-05-05 14:42:35 -0400

    homepage

---
+++ src/Components/AnimatedBackground.jsx
@@ -0,0 +1,13 @@
+import './AnimatedBackground.css'
+
+const AnimatedBackground = () => {
+	return (
+		<div className="main-page gradient-background">
+      
+      <h1> test gg</h1>
+  
+		</div>
+	)
+}
+
+export default AnimatedBackground
---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,13 @@
+import AnimatedBackground from './AnimatedBackground.jsx'
+
+const HomePage = () => {
+  return (
+    <div>
+      <AnimatedBackground>
+      
+      </AnimatedBackground>
+    </div>
+  );
+};
+
+export default HomePage;
